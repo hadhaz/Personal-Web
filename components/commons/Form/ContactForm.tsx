ff7e0490613ed0236b5d548d97b88e8f0e36659a
hadzami10 <achmad.had2003@mail.ugm.ac.id>
2022-09-14 17:30:34 +0700

    fix

--- components/commons/Form/ContactForm.tsx
+++ components/commons/Form/ContactForm.tsx
@@ -4,7 +4,7 @@ import { TouchApp } from "@mui/icons-material";
 import React, { useState } from "react";
 import validator from "validator";
 import Head from "next/head";
-import ReCAPTCHA from "react-google-recaptcha";
+import ReCAPTCHA, { ReCAPTCHAProps } from "react-google-recaptcha";
 
 type State = {
   email: string;
@@ -41,8 +41,6 @@ const Form = () => {
   const [error, setError] = useState<ErrorState>(initialErrorState);
   const recaptchaRef = createRef<ReCAPTCHA>();
 
-  function handleChange() {}
-
   function nameChangehandler(e: React.ChangeEvent<HTMLInputElement>) {
     setError((val: ErrorState) => ({ ...val, nameError: false }));
     setState((val: State) => ({ ...val, name: e.target.value }));
@@ -69,7 +67,7 @@ const Form = () => {
 
   function nameOnBlurHandler(e: React.ChangeEvent<HTMLInputElement>) {
     const name = e.target.value;
-    if (!validator.isEmpty(name)) {
+    if (validator.isEmpty(name)) {
       setError((val: ErrorState) => ({ ...val, nameError: true }));
     }
   }
@@ -95,16 +93,24 @@ const Form = () => {
     }
   }
 
-  function onReCAPTCHAChange() {}
+  function onReCAPTCHAChange(captchaCode: string | null) {
+    console.log(captchaCode);
+    if (!captchaCode) {
+      alert("Please Verify if You are Human");
+      return;
+    }
+    recaptchaRef.current?.reset();
+  }
 
   function submitFormHandler(event: React.FormEvent) {
     event.preventDefault();
+    recaptchaRef.current?.execute();
     const message = {
       name: state.name,
       email: state.email,
       phone: state.phone,
       organization: state.organization,
-      messages: state.messages,
+      messages: state.messages || "No Messages",
     };
 
     fetch("/api/email", {
@@ -122,6 +128,7 @@ const Form = () => {
     });
   }
 
+  const key = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || '';
   return (
     <Grid container item className='dark:bg-slate-800'>
       <div className='w-full py-8 md:py-12'>
@@ -134,123 +141,117 @@ const Form = () => {
           className='w-[95%] md:w-[500px] mx-auto flex flex-col gap-2'
           onSubmit={submitFormHandler}
         >
-          <div className='flex flex-col gap-1'>
-            <label
-              htmlFor='name'
-              className={error.nameError ? "text-red-500" : ""}
-            >
-              Name {error.nameError && "Required"}
-            </label>
-            <input
-              type='text'
-              id='name'
-              className={`border-2 bg-transparent ${
-                error.nameError
-                  ? "border-red-500"
-                  : "border-yellow-custom dark:border-blue-gray"
-              }  w-full outline-none py-1 px-2`}
-              placeholder='Achmad Hadzami Setiawan'
-              onChange={nameChangehandler}
-              onBlur={nameOnBlurHandler}
-              required
-              value={state.name}
-            />
-          </div>
-          <div className='flex flex-col gap-1'>
-            <label
-              htmlFor='phone'
-              className={error.phoneError ? "text-red-500" : ""}
-            >
-              Phone {error.phoneError && "is Not Valid"}
-            </label>
-            <input
-              type='text'
-              id='phone'
-              className={`${
-                error.phoneError
-                  ? "border-red-500"
-                  : "border-yellow-custom dark:border-blue-gray"
-              } bg-transparent border-2 w-full outline-none p-1 px-2`}
-              placeholder='+628123456789'
-              onChange={phoneChangehandler}
-              onBlur={phoneOnBlurHandler}
-              required
-              value={state.phone}
-            />
-          </div>
-          <div className='flex flex-col gap-1'>
-            <label
-              htmlFor='email'
-              className={error.emailError ? "text-red-500" : ""}
-            >
-              Email {error.emailError && "is Not Valid"}
-            </label>
-            <input
-              type='email'
-              id='email'
-              className={`${
-                error.emailError
-                  ? "border-red-500"
-                  : "border-yellow-custom dark:border-blue-gray"
-              } bg-transparent border-2 w-full outline-none p-1 px-2`}
-              placeholder='hadzami@example.com'
-              onChange={emailChangehandler}
-              onBlur={emailOnBlurHandler}
-              required
-              value={state.email}
-            />
-          </div>
-          <div className='flex flex-col gap-1'>
-            <label
-              htmlFor='institution'
-              className={error.organizationError ? "text-red-500" : ""}
-            >
-              Institution/Organization {error.organizationError && "Required"}
-            </label>
-            <input
-              type='text'
-              id='institution'
-              className={` ${
-                error.organizationError
-                  ? "border-red-500"
-                  : "border-yellow-custom dark:border-blue-gray"
-              } bg-transparent border-2 w-full outline-none p-1 px-2`}
-              placeholder='XYZ Universitas Gadjah Mada'
-              onChange={organizationChangehandler}
-              onBlur={organizationOnBlurHandler}
-              required
-              value={state.organization}
-            />
-          </div>
-          <div className='flex flex-col gap-1'>
-            <label htmlFor='text'>Additional Information</label>
-            <textarea
-              id='text'
-              onChange={messagesChangehandler}
-              value={state.messages}
-              className='bg-transparent border-2 border-yellow-custom dark:border-blue-gray w-full outline-none p-1 px-2 h-40'
-              placeholder='I will hire your for position x with jobdesc y'
-            />
-          </div>
           <ReCAPTCHA
-            sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
+            sitekey={key}
             onChange={onReCAPTCHAChange}
+            size='invisible'
             ref={recaptchaRef}
           >
-            <input
-              onChange={handleChange}
-              required
-              type='email'
-              name='email'
-              placeholder='Email'
-            />
+            <div className='flex flex-col gap-1'>
+              <label
+                htmlFor='name'
+                className={error.nameError ? "text-red-500" : ""}
+              >
+                Name {error.nameError && "Required"}
+              </label>
+              <input
+                type='text'
+                id='name'
+                className={`border-2 bg-transparent ${
+                  error.nameError
+                    ? "border-red-500"
+                    : "border-yellow-custom dark:border-blue-gray"
+                }  w-full outline-none py-1 px-2`}
+                placeholder='Achmad Hadzami Setiawan'
+                onChange={nameChangehandler}
+                onBlur={nameOnBlurHandler}
+                required
+                value={state.name}
+              />
+            </div>
+            <div className='flex flex-col gap-1'>
+              <label
+                htmlFor='phone'
+                className={error.phoneError ? "text-red-500" : ""}
+              >
+                Phone {error.phoneError && "is Not Valid"}
+              </label>
+              <input
+                type='text'
+                id='phone'
+                className={`${
+                  error.phoneError
+                    ? "border-red-500"
+                    : "border-yellow-custom dark:border-blue-gray"
+                } bg-transparent border-2 w-full outline-none p-1 px-2`}
+                placeholder='+628123456789'
+                onChange={phoneChangehandler}
+                onBlur={phoneOnBlurHandler}
+                required
+                value={state.phone}
+              />
+            </div>
+            <div className='flex flex-col gap-1'>
+              <label
+                htmlFor='email'
+                className={error.emailError ? "text-red-500" : ""}
+              >
+                Email {error.emailError && "is Not Valid"}
+              </label>
+              <input
+                type='email'
+                id='email'
+                className={`${
+                  error.emailError
+                    ? "border-red-500"
+                    : "border-yellow-custom dark:border-blue-gray"
+                } bg-transparent border-2 w-full outline-none p-1 px-2`}
+                placeholder='hadzami@example.com'
+                onChange={emailChangehandler}
+                onBlur={emailOnBlurHandler}
+                required
+                value={state.email}
+              />
+            </div>
+            <div className='flex flex-col gap-1'>
+              <label
+                htmlFor='institution'
+                className={error.organizationError ? "text-red-500" : ""}
+              >
+                Institution/Organization {error.organizationError && "Required"}
+              </label>
+              <input
+                type='text'
+                id='institution'
+                className={` ${
+                  error.organizationError
+                    ? "border-red-500"
+                    : "border-yellow-custom dark:border-blue-gray"
+                } bg-transparent border-2 w-full outline-none p-1 px-2`}
+                placeholder='XYZ Universitas Gadjah Mada'
+                onChange={organizationChangehandler}
+                onBlur={organizationOnBlurHandler}
+                required
+                value={state.organization}
+              />
+            </div>
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='text'>Additional Information</label>
+              <textarea
+                id='text'
+                onChange={messagesChangehandler}
+                value={state.messages}
+                className='bg-transparent border-2 border-yellow-custom dark:border-blue-gray w-full outline-none p-1 px-2 h-40'
+                placeholder='I will hire your for position x with jobdesc y'
+              />
+            </div>
+            <button
+              type='submit'
+              className='bg-yellow-custom w-full dark:bg-blue-gray p-2 dark:text-sky-900 font-bold rounded-sm mt-2'
+            >
+              Send to Hadzami
+            </button>
           </ReCAPTCHA>
-          <button
-            type='submit'
-            className='bg-yellow-custom dark:bg-blue-gray p-2 dark:text-sky-900 font-bold rounded-sm mt-2'
-          >
-            Send to Hadzami
-          </button>
         </form>
       </div>
     </Grid>
